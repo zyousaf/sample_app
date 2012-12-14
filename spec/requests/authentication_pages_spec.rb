a0b9f88d268a3af68b69a19368e9261e451935bd
Zain Yousaf <zain@zainyousaf.com>
2012-12-14 13:58:26 -0500

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -67,6 +67,18 @@ describe "with valid information" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
       
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { response.should redirect_to(signin_path) }          
+        end
+      end
+      
       describe "in the Microposts controller" do
 
         describe "submitting to the create action" do
@@ -97,7 +109,6 @@ describe "with valid information" do
       end
 
       describe "in the Users controller" do
-        
         describe "visiting the user index" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
@@ -111,6 +122,16 @@ describe "with valid information" do
         describe "submitting to the update action" do
           before { put user_path(user) }
           specify { response.should redirect_to(signin_path) }
+        end
+        
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
         end
       end
     end
