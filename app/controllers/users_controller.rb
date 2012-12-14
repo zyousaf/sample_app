82d0e4dcbb571f2abd2e0967ce86452f83e5bb33
Zain Yousaf <zain@zainyousaf.com>
2012-12-13 19:43:53 -0500

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
@@ -44,16 +45,8 @@ def update
       render 'edit'
     end
   end
- 
   
 private
-  
-def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
 
     def correct_user
       @user = User.find(params[:id])
